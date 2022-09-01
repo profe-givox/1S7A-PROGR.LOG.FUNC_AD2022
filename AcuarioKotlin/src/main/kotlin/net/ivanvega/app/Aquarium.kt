db81f067db3b92cb99127d5560af315899c67b34
el givo <gi.vega@itsur.edu.mx>
2022-09-01 12:54:31 -0500

    Se agrego la clase Acuario

--- AcuarioKotlin/src/main/kotlin/net/ivanvega/app/Aquarium.kt
+++ AcuarioKotlin/src/main/kotlin/net/ivanvega/app/Aquarium.kt
@@ -1,4 +1,33 @@
 package net.ivanvega.app
 
 class Aquarium {
+    var width: Int =20
+    var heigth: Int = 40
+    var length: Int = 100
+    var volume: Int = 0
+    init {
+        volume = width*heigth*length
+    }
+
+    fun printSize(){
+        println("Ancho: $width, " + "alto: $heigth, " + "largo:  $length " )
+        println("Volume: $volume " )
+    }
+}
+
+fun construirAquario(){
+    val miAcuario = Aquarium()
+    miAcuario.printSize()
+    println("-----------------------------")
+
+    miAcuario.heigth = 70
+    miAcuario.printSize()
+
+    println("-----------------------------")
+    miAcuario.volume =  90000
+    miAcuario.printSize()
+}
+
+fun main(){
+    construirAquario()
 }
