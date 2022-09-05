631edd07dbb597c88527a90c92acbfe137904088
el givo <gi.vega@itsur.edu.mx>
2022-09-05 13:01:50 -0500

    Constructores en Kotlin

--- AcuarioKotlin/src/main/kotlin/net/ivanvega/app/Aquarium.kt
+++ AcuarioKotlin/src/main/kotlin/net/ivanvega/app/Aquarium.kt
@@ -1,12 +1,20 @@
 package net.ivanvega.app
 
-class Aquarium {
-    var width: Int =20
-    var heigth: Int = 40
-    var length: Int = 100
+class Aquarium ( width: Int =20, heigth: Int = 40,  length: Int = 100 ) {
+    var width: Int = width
+    var heigth: Int = heigth
+    var length: Int = length
     var volume: Int = 0
     init {
+        println("Bloque de inicializacion")
         volume = width*heigth*length
+        printSize()
+    }
+
+    constructor(numeroDePeces: Int) : this() {
+        val estanque = numeroDePeces * 2000 * 1.1
+        println((estanque / (length*width)))
+        heigth =  (estanque / (length*width)).toInt()
     }
 
     fun printSize(){
@@ -26,6 +34,23 @@ fun construirAquario(){
     println("-----------------------------")
     miAcuario.volume =  90000
     miAcuario.printSize()
+
+    println("-----------------------------")
+    val aq2 = Aquarium(90, 110, 1000)
+    aq2.printSize()
+
+    println("-----------------------------")
+    val aq3 = Aquarium(  heigth = 1000)
+    aq3.printSize()
+
+    println("-----------------------------")
+    val aq4 = Aquarium(  )
+
+    println("-----------------------------")
+    val aq5 = Aquarium( numeroDePeces = 3  )
+    aq5.printSize()
+    println( aq5.heigth)
+
 }
 
 fun main(){
