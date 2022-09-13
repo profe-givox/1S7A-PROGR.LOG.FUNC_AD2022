e855329d6d394bdee0683f4ebd693f1c8c12eccf
el givo <gi.vega@itsur.edu.mx>
2022-09-13 12:57:20 -0500

    DELEGACION CON by

--- AcuarioKotlin/src/main/kotlin/net/ivanvega/app/AquariumFish.kt
+++ AcuarioKotlin/src/main/kotlin/net/ivanvega/app/AquariumFish.kt
@@ -1,17 +1,37 @@
 package net.ivanvega.app
 
+import com.sun.deploy.config.Config
+
 abstract class AquariumFish {
     abstract val color : String
 }
+
 
 fun makeFish(){
     val shark = Shark();
     println("Shark color: ${shark.color}")
     shark.eat()
 
-    val plecus = Plecostumes()
-    println("Plecus color: ${plecus.color}")
+    val plecus = Plecostumes(
+        object : FishColor {
+            override val color: String
+                get() = "verde"
+
+        }
+    )
+    println("Plecus 1 color: ${plecus.color}")
     plecus.eat()
+
+    val plecus2 = Plecostumes(
+        object : FishColor {
+            override val color: String
+                get() = "verde"
+
+        }
+    )
+    println("Plecus 2 color: ${plecus.color}")
+    plecus.eat()
+
 }
 
 fun main(){
--- AcuarioKotlin/src/main/kotlin/net/ivanvega/app/Plecostumes.kt
+++ AcuarioKotlin/src/main/kotlin/net/ivanvega/app/Plecostumes.kt
@@ -1,8 +1,9 @@
 package net.ivanvega.app
 
 //class Plecostumes : AquariumFish(), FishAction {
-class Plecostumes : FishColor, FishAction {
-    override val color: String = GoldColor.color
+//class Plecostumes : FishAction, FishColor by GoldColor  {
+class Plecostumes (fishColor: FishColor = GoldColor) : FishAction, FishColor by  fishColor  {
+//    override val color: String = GoldColor.color
     override fun eat() {
         println("Eat algae")
     }
